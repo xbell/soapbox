91d317509c585bd309de758331ca7a255cc01727
Evelyn <evieb16@gmail.com>
2015-10-15 22:49:29 -0700

    made save_bubble method

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'bubble'
 
+
 puts "What's your username?"
 username = gets.chomp
 puts "-" * 75
@@ -7,10 +8,10 @@ puts "-" * 75
 body = ""
 bubble_array = []
 
-while body != "nothing"
-  puts "What's on your mind? Type \"nothing\" when finished."
+while body != "Exit"
+  puts "What's on your mind? Type \"Exit\" when finished."
   body = gets.chomp
-  break if body == "nothing"
+  break if body.capitalize == "Exit"
 
   puts "-" * 75
 
@@ -22,15 +23,28 @@ while body != "nothing"
 
   bubble = Bubble.new(bubble_info)
 
-  save_bubble = File.open("/Users/ebell/Dropbox/SoapBox/" + bubble.body, "w")
-  save_bubble.write(bubble.username)
-  save_bubble.close
+  bubble.save_bubble
+
+  # save_bubble = File.open("/Users/ebell/Dropbox/SoapBox/" + bubble.body, "w")
+  # save_bubble.write(bubble.username)
+  # save_bubble.close
 
   bubble_array.push(bubble)
 
   bubble_array.each do |bubble|
     bubble.print_info
     puts "-" * 75
+
+    # all_bubbles = Dir.glob("/Users/ebell/Dropbox/SoapBox/*")
+
+  # puts "To post again, type \"Post\". Or type \"Refresh\" to refesh."
+  # response = gets.chomp
+  #
+  # if response.capitalize == "Post" || response.capitalize =="Refresh"
+  #   puts "-" * 75
+  # else
+  #   puts "Sorry I didn't get that."
+  # end
   end
 end
 
--- bubble.rb
+++ bubble.rb
@@ -12,7 +12,13 @@ class Bubble
     def print_info
       puts @username
       puts @body
-      puts @created_at
+      puts @created_at.strftime("%A, %d %b %Y %l:%M %p")
+    end
+
+    def save_bubble
+      new_file = File.open("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/Bubbles/#{@body}", "w")
+      new_file.write(@username)
+      new_file.close
     end
 
 end
