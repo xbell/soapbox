b65909282c626e63cf8a23af18b1ce605489fa69
Evelyn <evieb16@gmail.com>
2015-10-18 17:44:32 -0700

    added prompt username function, added logout feature

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@ require_relative 'bubble'
 require_relative 'soapboxfiles'
 require 'colorize'
 
+def prompt_username
+  puts "What's your username?".green
+  username = gets.chomp
+  puts ("-" * 75).blue
+  # save username to .soapbox file
+  username_file = File.open("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/.soapbox", "w")
+  username_file.write(username)
+  username_file.close
+  username
+end
+
 puts "welcome to soapbox".upcase.bold.magenta.underline
 username = ""
 # check if .soapbox file exists
@@ -10,13 +21,7 @@ if File.exist?("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/.soapbox")
   username_file = File.open("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/.soapbox")
   username = username_file.read
 else
-  puts "What's your username?".green
-  username = gets.chomp
-  puts ("-" * 75).blue
-  # save username to .soapbox file
-  username_file = File.open("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/.soapbox", "w")
-  username_file.write(username)
-  username_file.close
+  username = prompt_username
 end
 
 body = ""
@@ -41,7 +46,7 @@ while body != "Exit"
     bubble_array.push(bubble)
   end
 
-  puts "What's on your mind? Press enter to Post, or type \"Refresh\" to refresh, or \"Exit\" when finished.".green
+  puts "What's on your mind? Press enter to Post, or type \"Refresh\" to refresh, \"Logout\" to logout of your current username, or \"Exit\" when finished.".green
   body = gets.chomp
   break if body.capitalize == "Exit"
   if body.capitalize == "Refresh"
@@ -49,6 +54,12 @@ while body != "Exit"
       bubble.print_info
       puts ("-" * 75).blue
     end
+    next
+  end
+
+  if body.capitalize == "Logout"
+    File.delete("/Users/ebell/Code_Builders/Git_Hub_Work/soapbox/.soapbox")
+    username = prompt_username
     next
   end
 
